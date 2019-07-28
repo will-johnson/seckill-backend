0359ab6647a311908270dd0b972e544014516d4e
张亚森 <867258977@qq.com>
2019-07-28 13:23:33 +0800

    update diagram

--- src/main/java/com/seen/seckillbackend/common/response/GlobalExceptionHandler.java
+++ src/main/java/com/seen/seckillbackend/common/response/GlobalExceptionHandler.java
@@ -5,11 +5,12 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.concurrent.ThreadPoolExecutor;
+
 /**
  * @ControllerAdvice 注解，可以用于定义@ExceptionHandler、@InitBinder、@ModelAttribute，并应用到所有@RequestMapping中。
  */
 @ControllerAdvice
-@Slf4j
 public class GlobalExceptionHandler  {
 
     @ExceptionHandler(value = Exception.class)
